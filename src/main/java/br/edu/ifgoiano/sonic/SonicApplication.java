0ae65c82ead8d675761665cfa0906e524991373a
Adamo Gonçalves Maia <87905208+yKronoos@users.noreply.github.com>
2022-06-25 23:19:12 -0300

    Interprise Sonic

---
+++ src/main/java/br/edu/ifgoiano/sonic/SonicApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.sonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonicApplication.class, args);
+	}
+
+}
